89bec34f78849cbfe9add07b5da9d4dfdbeb8dc5
edimar <edimar@edimar-OptiPlex-760>
2016-06-15 11:15:31 -0300

    Testando git pelo netbeans

--- Teste02/src/teste02/Teste02.java
+++ Teste02/src/teste02/Teste02.java
@@ -16,7 +16,7 @@ public class Teste02 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Olá");
+        System.out.println("Olá Mundo");
     }
     
 }
